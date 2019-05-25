b9212d83b9f9af5f013f2e44a93151d71ecf4183
guanyingjie <39229309+guanyingjie@users.noreply.github.com>
2019-05-25 15:15:08 +0800

    first commit

---
+++ src/main/java/com/Employee/EmployeeCURD/EmployeeCurdApplication.java
@@ -0,0 +1,13 @@
+package com.Employee.EmployeeCURD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCurdApplication.class, args);
+	}
+
+}
